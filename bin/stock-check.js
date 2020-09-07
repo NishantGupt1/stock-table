a9fa5cd776f9deeb0fb951ae5cae031c2c9e9096
Nishant Gupt <nishantgupt1@gmail.com>
2020-09-07 15:44:02 -0400

    deprecated

---
+++ bin/stock-check.js
@@ -0,0 +1,20 @@
+// const program = require('commander');
+// const check = require('../commands/check');
+
+// program
+//   .command('price')
+//   .description('Get the stock price')
+//   .option(
+//     '--ticker <type>',
+//     'Stock ticker you would like to check',
+//     'MSFT'
+//   )
+//   .action(cmd => {
+//     check.price(cmd)});
+
+// program.parse(process.argv);
+
+// // If no args, output help
+// if (!process.argv[2]) {
+//   program.outputHelp();
+// }
